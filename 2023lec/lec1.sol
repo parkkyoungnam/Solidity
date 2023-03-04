4ccaad180054064841a5de5157d1aa9151aa9491
parkkyoungnam <pkn0813@naver.com>
2023-03-04 18:54:34 +0900

    test

---
+++ 2023lec/lec1.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Hello
+{
+    uint public a = 3;
+
+
+    function increace() public
+    {
+        a ++;
+    }
+
+    function output(uint b) public returns (uint)
+    {
+        return a;
+    }
+
+
+}
